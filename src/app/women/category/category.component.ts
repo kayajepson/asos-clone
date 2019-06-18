f87558122f2fe42664bb1c25a4d54b9fc7f142ff
brooke kullberg and kaya jepson <brooke@>
2019-06-18 11:26:49 -0700

    retrieve data in dynamic route

--- src/app/women/category/category.component.ts
+++ src/app/women/category/category.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-category',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
+  selectedCat: string = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.selectedCat = urlParameters['category']
+    })
   }
 
 }

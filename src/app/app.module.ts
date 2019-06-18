b94765fa98ad7acb02e7260a3042c6ae4b3b98ec
brooke kullberg and kaya jepson <kaya@>
2019-06-18 09:21:28 -0700

    initial files and welcome component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
-
+import { WelcomeComponent } from './welcome/welcome.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+   HttpModule,
+   routing
   ],
   providers: [],
   bootstrap: [AppComponent]
